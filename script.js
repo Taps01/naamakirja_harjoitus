1d201a860a008207271eca3ad0db3bcf0cdcd037
Lauri <phantom.loop.games@gmail.com>
2020-12-10 13:00:07 +0200

    base push

---
+++ script.js
@@ -0,0 +1,22 @@
+var database = [
+{
+    username: "Lauri",
+    password: "salainensana",
+}
+
+];
+
+var newsFeed = [
+{
+    username: "Keijo",
+    timeline: "Kello on Keijo",
+},
+{
+    username: "Salli",
+    timeline: "Jejejejeee",
+}
+
+];
+
+var userNamePrompt = prompt("What is your username?");
+var passwordPrompt = prompt("what is your password?");
