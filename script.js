d0053371c4809781c04bd6eca060c722ff41a442
Lauri <phantom.loop.games@gmail.com>
2020-12-10 13:19:27 +0200

    login info

--- script.js
+++ script.js
@@ -20,3 +20,13 @@ var newsFeed = [
 
 var userNamePrompt = prompt("What is your username?");
 var passwordPrompt = prompt("what is your password?");
+
+function signIn(username, password) {
+    if (username === database[0], username && password === database[0].password) {
+        console.log(newsFeed);
+    } else {
+        alert("Access deniaded!");
+    }
+}
+
+signIn(userNamePrompt, passwordPrompt);
