3048bdc262a3de491980798ff723daa8b7f7f449
Lauri <phantom.loop.games@gmail.com>
2020-12-10 14:09:17 +0200

    login korjailtu

--- script.js
+++ script.js
@@ -2,7 +2,19 @@ var database = [
 {
     username: "Lauri",
     password: "salainensana",
-}
+},
+{
+    username: "Sally",
+    password: "qwerty",
+},
+{
+    username: "Kalle",
+    password: "1234",
+},
+{
+    username: "Reetta",
+    password: "qazwsx",
+},
 
 ];
 
@@ -18,15 +30,29 @@ var newsFeed = [
 
 ];
 
-var userNamePrompt = prompt("What is your username?");
-var passwordPrompt = prompt("what is your password?");
+
+function isUserValid(username, password) {
+    for (var i=0; i < database.length; i++) {
+        if(database[i].username === username && database[i].password === password) {
+            return true;
+        } else {
+            alert("Sorry wrong username and password.");
+        }
+        return false;
+}
+}
 
 function signIn(username, password) {
-    if (username === database[0], username && password === database[0].password) {
+    console.log(isUserValid(username, password)); {
+    }
+    if (isUserValid(username, password)) {
         console.log(newsFeed);
     } else {
-        alert("Access deniaded!");
+        alert("Access denied!");
     }
 }
+
+var userNamePrompt = prompt("What is your username?");
+var passwordPrompt = prompt("what is your password?");
 
 signIn(userNamePrompt, passwordPrompt);
